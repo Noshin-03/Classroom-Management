af3e2f625364ae239a1ed2ac0a26bfb0bf55ca85
Noshin <bsse1503@iit.du.ac.bd>
2026-02-28 14:14:02 +0600

    Backend setup - models, migrations, sequelize config

---
+++ backend/migrations/20260228080847-create-subject.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Subjects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      department_id: {
+        type: Sequelize.INTEGER
+      },
+      teacher_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Subjects');
+  }
+};
